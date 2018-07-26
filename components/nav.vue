2ac1b8b2908a3e81e42cfeacea928c82033d7172
unknown <igor.souza96@hotmail.com>
2018-07-26 20:51:40 -0300

    feat: first commit, netlify host, test version

---
+++ components/nav.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="nav-component">
+    <div class="container nav">
+      <div class="left">
+        <h1><a href="/">Igor Martins</a></h1>
+        <p>photographer</p>
+      </div>
+      <div class="right">
+        <p><a href="#">overview</a></p>
+        <p><a href="#">projects</a></p>
+        <p><a href="#">social</a></p>
+        <p><a href="#">about + contact</a></p>
+      </div>
+    </div>
+  </div>
+</template>
+<style>
+.nav-component .nav {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+    align-items: center;
+
+    height: 20vh;
+}
+.nav-component .nav .left h1 {
+    font-family: 'Pacifico', cursive;
+    font-size: 30px;
+}
+.nav-component .nav .left a, .nav-component .nav .left a:hover {
+    color: #6c5ce7 !important;
+}
+.nav-component .nav .left p {
+    color: #777;
+}
+.nav-component .nav .right {
+    display: flex;
+    justify-content: space-between;
+    flex-direction: row;
+}
+.nav-component .nav .right > p {
+    margin: 10px;
+    /* font-family: 'Montserrat', sans-serif; */
+    font-family: 'Amatic SC', cursive;
+    font-size: 25px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="principal">
+    <nav-top />
+
+    <div class="container thumbnails fade">
+      <div class="thumb"><img src="/images/1.png" alt="" class="img-fluid opacity"></div>
+      <div class="thumb"><img src="/images/2.png" alt="" class="img-fluid opacity"></div>
+      <div class="thumb"><img src="/images/3.png" alt="" class="img-fluid opacity"></div>
+      <div class="thumb"><img src="/images/4.png" alt="" class="img-fluid opacity"></div>
+      <div class="thumb"><img src="/images/5.png" alt="" class="img-fluid opacity"></div>
+      <div class="thumb"><img src="/images/6.png" alt="" class="img-fluid opacity"></div>
+    </div>
+
+    <div class="spacer container all-projects">
+      <a href="#">See all projects</a>
+    </div>
+  </div>
+</template>
+<style>
+.principal .thumbnails {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+
+  opacity: 0;
+}
+.principal .thumbnails > .thumb {
+  width: 33%;
+  padding: 25px;
+}
+.principal .all-projects {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.principal .all-projects a {
+  font-family: 'Montserrat', sans-serif;
+  font-weight: 500;
+
+  background: #2d3436;
+  padding: 20px;
+  text-transform: uppercase;
+  color: #fff;
+}
+</style>
+<script>
+import navTop from '@/components/nav'
+
+export default {
+    components: {
+      navTop
+    }
+}
+</script>
+
